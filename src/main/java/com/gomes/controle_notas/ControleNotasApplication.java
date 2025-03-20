02427454f1c1f029c4a13d1e028ee31913ab9f1a
Gustavo Gomes <gugags2@hotmail.com>
2025-03-20 02:31:18 -0300

    first commit

---
+++ src/main/java/com/gomes/controle_notas/ControleNotasApplication.java
@@ -0,0 +1,13 @@
+package com.gomes.controle_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleNotasApplication.class, args);
+	}
+
+}
